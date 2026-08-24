101595b4380609c1668ed9a9ffb8103faaf86ae3
sagar7162 <sagar180choudhary@gmail.com>
2026-08-25 00:41:47 +0530

    add auth routers

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@
 
 from fastapi import FastAPI
 
+from app.routers.auth_router import router as auth_router
+
 
 app = FastAPI(title="FlagBoard")
+app.include_router(auth_router)
 
 
 @app.get("/health")
